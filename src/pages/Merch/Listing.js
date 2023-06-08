ee8bb4f5bb4ff194a7d66e9c7b5dc3e7610ff0b1
martinborys <marcinb117@gmail.com>
2023-06-08 13:42:54 -0400

    merch styling

--- src/pages/Merch/Listing.js
+++ src/pages/Merch/Listing.js
@@ -1,13 +1,17 @@
+import "./merch.css";
+
 export default function Listing({ product }) {
   return (
-    <div>
+    <div className="product-container">
       <img
         src={`../../assets/${product.imageFile.front}`}
         alt={product.imageFile.front}
+        width={"200px"}
+        height={"200px"}
       />
-      <div>
+      <div className="product-details">
         <h2>{product.name}</h2>
-        <span>{product.priceInCents}</span>
+        <span>{`$${product.priceInDollars}`}</span>
       </div>
     </div>
   );
